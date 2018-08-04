f660a0ed4a6ea75632703eb12aa9a07cd5317957
Ansh Jain <ansh.jain1@outlook.com>
2018-08-04 12:32:48 -0500

    Grammar Commit 2
    Changed around the rename script to finish renaming all grammar files and folders.

--- PowerShell/Grammar/RenameGrammar.ps1
+++ PowerShell/Grammar/RenameGrammar.ps1
@@ -1,15 +1,15 @@
 ﻿#----------------------------------------------------------------------------------------------------------
 #RenameAlgebra.ps1
-#Goal of this script is to rename Algebra book files by placing the hyphen in the correct place for parsing
+#Goal of this script is to rename Grammar book files by placing the hyphen in the correct place for parsing
 #----------------------------------------------------------------------------------------------------------
-$source = "C:\Users\QLOWORKSTATION5\Mr. Ansh\PowerShell and Batch\Online Master\Grammar\Quest Grammar 2\Grammar 2A\1 page"
+$source = "C:\Users\QLOWORKSTATION5\Mr. Ansh\PowerShell and Batch\Online Master\Grammar\Grammar 6\Grammar 6R"
 $sourceFiles = Get-ChildItem -Path $source -Recurse -ErrorAction SilentlyContinue -Force | Get-ChildItem -File
 foreach($file in $sourceFiles) {
-    if(!($file -like '*TEST*') -and !($file -like '*BOOK*') -and !($file -like '*h.pdf*')) {
+    if(!($file -like '*TEST*') -and !($file -like '*BOOK*') -and !($file -like '*Link*') -and !($file -like '*6 R*')) {
         $initialPath = $file.FullName
         $splitFile = $file -split "\\"
         $oldName = $splitFile[$splitFile.Length - 1]
-        $newName = "G" + $oldName.Substring(1, 8) + $oldName.Substring(10, 1) + "- " + $oldName.Substring(12)
+        $newName = "G" + $oldName.Substring(1, 8) + $oldName.Substring(9, 1) + "- " + $oldName.Substring(11)
         $newName
         Rename-Item $initialPath -NewName $newName
     }
