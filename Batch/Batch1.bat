7c7fe2410a84a47dfc80246619cd37b41fec48a7
Ansh Jain <ansh.jain1@outlook.com>
2018-09-15 12:36:43 -0500

    Testing

--- Batch/Batch1.bat
+++ Batch/Batch1.bat
@@ -51,32 +51,29 @@ if defined array[%x%] (
 	set /a "x+=1"
 	GOTO :SymLoop
 )
-
 echo The length of the array is %x%
 
 ::How to create structures within Arrays
 goto :pastStructureArray
-set len=3 
-set obj[0].Name=Joe 
-set obj[0].ID=1 
-set obj[1].Name=Mark 
-set obj[1].ID=2 
-set obj[2].Name=Mohan 
-set obj[2].ID=3 
-set i=0 
-:loop 
+set len=3
+set obj[0].Name=Joe
+set obj[0].ID=1
+set obj[1].Name=Mark
+set obj[1].ID=2
+set obj[2].Name=Mohan
+set obj[2].ID=3
+set i=0
+:loop
 
-if %i% equ %len% goto :eof 
-set cur.Name= 
+if %i% equ %len% goto :eof
+set cur.Name=
 set cur.ID=
 
-for /f "usebackq delims==.tokens=1-3" %%j in (`set obj[%i%]`) do ( 
-   set cur.%%k=%%l 
-) 
-echo Name=%cur.Name% 
-echo Value=%cur.ID% 
-set /a i=%i%+1 
+for /f "usebackq delims==.tokens=1-3" %%j in (`set obj[%i%]`) do (
+   set cur.%%k=%%l
+)
+echo Name=%cur.Name%
+echo Value=%cur.ID%
+set /a i=%i%+1
 goto loop
 :pastStructureArray
-
-
